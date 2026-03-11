6f1b41c77687c7ca21db0619ad516bc6012e58e4
delight15 <dumnamenefyneface@gmail.com>
2026-03-11 10:10:09 +0100

    passwordgenerator.java

---
+++ returningarraysfrommethods.java
@@ -0,0 +1,14 @@
+public class returningarraysfrommethods{
+
+	public static void main(String[] args){
+		for(int i = 0; i < 5; i++){
+			
+			System.out.println(test()[i]);
+			
+		}
+	}
+	public static int[] test(){
+		
+		return new int[]{6,9,12,15,4};
+	}
+}
